26ca7112ebfafcd11e45b92465be4b0f47e0bfc9
HELSON <c2h214748@gmail.com>
2023-03-02 16:28:05 +0800

    [chunkgroup] add chunk group

---
+++ elixir/chunk/chunkgroup.py
@@ -0,0 +1,19 @@
+class ChunkGroup(object):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def allocate_chunk():
+        pass
+
+    def access_chunk():
+        pass
+
+    def release_chunk():
+        pass
+
+    def reduce_chunk():
+        pass
+
+    def prefetch_chunk():
+        pass
