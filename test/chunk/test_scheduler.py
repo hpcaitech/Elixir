9055615dbc1ce65bb1503769639d75ff55dbbfb5
HELSON <c2h214748@gmail.com>
2023-03-09 17:04:53 +0800

    [scheduler] add unit test for prefetch scheduler

--- test/chunk/test_scheduler.py
+++ test/chunk/test_scheduler.py
@@ -7,7 +7,7 @@ import torch.distributed as dist
 
 from elixir import init_distributed
 from elixir.chunk import Chunk, MemoryPool
-from elixir.chunk.scheduler import FIFOScheduler
+from elixir.chunk.scheduler import FIFOScheduler, PrefetchScheduler
 
 
 def exam_fifo(nproc, group):
@@ -39,6 +39,71 @@ def exam_fifo(nproc, group):
     assert sdl.top() == c0
 
 
+def exam_prefetch(nproc, group):
+    mp = MemoryPool('cuda')
+    mp.allocate()
+    c0 = Chunk(mp, 1024, torch.float, group)
+    c1 = Chunk(mp, 1024, torch.float, group)
+    c2 = Chunk(mp, 1024, torch.float, group)
+
+    chunk_called_per_step = [[c0], [c1], [c2], [c0], [c0], [c1], [c2], [c2], [c1], [c0]]
+
+    sdl = PrefetchScheduler(process_list=chunk_called_per_step)
+    print(sdl.next_step_dict)
+    sdl.reset()
+
+    sdl.step()
+    sdl.add(c0)
+    assert sdl.top() == c0
+
+    sdl.step()
+    sdl.add(c1)
+    assert sdl.top() == c1
+
+    sdl.step()
+    sdl.add(c2)
+    assert sdl.top() == c2
+
+    sdl.remove(c0)
+    sdl.step()
+    sdl.add(c0)
+    assert sdl.top() == c2
+
+    sdl.remove(c0)
+    sdl.step()
+    sdl.add(c0)
+    assert sdl.top() == c0
+    sdl.remove(c0)    # notice here
+
+    sdl.remove(c1)
+    sdl.step()
+    sdl.add(c1)
+    assert sdl.top() == c1
+
+    sdl.remove(c2)
+    sdl.step()
+    sdl.add(c2)
+    assert sdl.top() == c1
+
+    sdl.remove(c2)
+    sdl.step()
+    sdl.add(c2)
+    assert sdl.top() == c2
+    sdl.remove(c2)    # notice here
+    sdl.add(c0)    # notice here
+
+    sdl.remove(c1)
+    sdl.step()
+    sdl.add(c1)
+    assert sdl.top() == c1
+    sdl.remove(c1)    # notice here
+
+    sdl.remove(c0)
+    sdl.step()
+    sdl.add(c0)
+    assert sdl.top() == c0
+
+
 def run_dist(rank, world_size):
     os.environ['RANK'] = str(rank)
     os.environ['LOCAL_RANK'] = str(rank)
@@ -47,6 +112,7 @@ def run_dist(rank, world_size):
     os.environ['MASTER_PORT'] = str(29512)
     init_distributed()
     exam_fifo(nproc=world_size, group=dist.GroupMember.WORLD)
+    exam_prefetch(nproc=world_size, group=dist.GroupMember.WORLD)
 
 
 @pytest.mark.dist
