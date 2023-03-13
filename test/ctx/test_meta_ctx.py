04c99e6d5ef84e12ef88a636ead86dba6ba699e0
HELSON <c2h214748@gmail.com>
2023-03-13 15:23:07 +0800

    [tests] add assertion for meta context test (#1)

--- test/ctx/test_meta_ctx.py
+++ test/ctx/test_meta_ctx.py
@@ -1,22 +1,21 @@
 from test.utils import TEST_MODELS
 
 from elixir.ctx import MetaContext
-from elixir.tracer.param_tracer import generate_fx_order
 
 
-def main():
+def test_meta_context():
     builder, *_ = TEST_MODELS.get_func('resnet')()
     with MetaContext():
         model = builder()
 
     for name, param in model.named_parameters():
+        assert param.device.type == 'meta'
         print(name, param)
 
     for name, buffer in model.named_buffers():
+        assert buffer.device.type == 'meta'
         print(name, buffer)
-
-    print(generate_fx_order(model))
 
 
 if __name__ == '__main__':
-    main()
+    test_meta_context()
