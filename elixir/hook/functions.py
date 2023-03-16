281277f7a237f265e981c032a9963d84df5ffafb
HELSON <c2h214748@gmail.com>
2023-03-16 20:04:18 +0800

    update functions in hook

--- elixir/hook/functions.py
+++ elixir/hook/functions.py
@@ -2,14 +2,27 @@ import torch
 
 from elixir.chunk import ChunkFetcher
 
+from .storage import BufferStore
 
-def prefwd_postbwd_function(fetcher: ChunkFetcher):
+
+def prefwd_postbwd_function(fetcher: ChunkFetcher, store: BufferStore):
 
     class PreFwdPostBwd(torch.autograd.Function):
 
         @staticmethod
         def forward(ctx, params, *args):
-            ctx.params = params
+            with torch._C.DisableTorchFunction():
+                ctx.params = params
+                chunks = fetcher.trans_to_compute(params)
+                fetcher.fetch_chunks(chunks)
+
+                offset = 0
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        # we should add parameters to buffer
+                        # because their blocks may be changed
+                        offset = store.insert(p, offset)
+
             return args
 
         @staticmethod
@@ -20,23 +33,29 @@ def prefwd_postbwd_function(fetcher: ChunkFetcher):
                     print(torch.sum(g), end=' ')
                 print('')
                 fetcher.trans_to_hold(ctx.params, phase='b')
-                return (None, *grads)
 
-    def exec_func(params, *args):
-        chunks = fetcher.trans_to_compute(params)
-        fetcher.fetch_chunks(chunks)
-        return PreFwdPostBwd.apply(params, *args)
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        store.erase(p)
 
-    return exec_func
+            return (None, *grads)
+
+    return PreFwdPostBwd.apply
 
 
-def postfwd_prebwd_function(fetcher: ChunkFetcher):
+def postfwd_prebwd_function(fetcher: ChunkFetcher, store: BufferStore):
 
     class PostFwdPreBwd(torch.autograd.Function):
 
         @staticmethod
         def forward(ctx, params, *args):
-            ctx.params = params
+            with torch._C.DisableTorchFunction():
+                ctx.params = params
+
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        store.erase(p)
+
             return args
 
         @staticmethod
@@ -44,6 +63,14 @@ def postfwd_prebwd_function(fetcher: ChunkFetcher):
             with torch._C.DisableTorchFunction():
                 chunks = fetcher.trans_to_compute(ctx.params)
                 fetcher.fetch_chunks(chunks)
+
+                offset = 0
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        # we should add parameters to buffer
+                        # because their blocks may be changed
+                        offset = store.insert(p, offset)
+
                 print('grad before', end=' ')
                 for g in grads:
                     print(torch.sum(g), g.data_ptr(), end=' ')
@@ -56,10 +83,6 @@ def postfwd_prebwd_function(fetcher: ChunkFetcher):
                 for g in grads:
                     print(torch.sum(g), end=' ')
                 print('')
-                return (None, *grads)
+            return (None, *grads)
 
-    def exec_func(params, *args):
-        fetcher.trans_to_hold(params, phase='f')
-        return PostFwdPreBwd.apply(params, *args)
-
-    return exec_func
+    return PostFwdPreBwd.apply
