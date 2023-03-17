[hook] fix gradient erorr with buffer (#12)
* [test] do not merge

* [hook] add BufferStorage class

* update functions in hook

* update parameter in hook

* [hook] fix backward error with buffer

* polish code

elixir/hook/functions.py
@@ -2,40 +2,72 @@ import torch
 
 from elixir.chunk import ChunkFetcher
 
+from .storage import BufferStore
 
-def prefwd_postbwd_function(fetcher: ChunkFetcher):
+
+def prefwd_postbwd_function(fetcher: ChunkFetcher, store: BufferStore):
 
     class PreFwdPostBwd(torch.autograd.Function):
 
         @staticmethod
         def forward(ctx, params, *args):
-            ctx.params = params
-            chunks = fetcher.trans_to_compute(params)
-            fetcher.fetch_chunks(chunks)
+            with torch._C.DisableTorchFunction():
+                ctx.params = params
+                chunks = fetcher.trans_to_compute(params)
+                fetcher.fetch_chunks(chunks)
+
+                offset = 0
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        # we should add parameters to buffer
+                        # because their blocks may be changed
+                        offset = store.insert(p, offset)
+
             return args
 
         @staticmethod
         def backward(ctx, *grads):
-            fetcher.trans_to_hold(ctx.params, phase='b')
+            with torch._C.DisableTorchFunction():
+                fetcher.trans_to_hold(ctx.params, phase='b')
+
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        store.erase(p)
+
             return (None, *grads)
 
-    return PreFwdPostBwd
+    return PreFwdPostBwd.apply
 
 
-def postfwd_prebwd_function(fetcher: ChunkFetcher):
+def postfwd_prebwd_function(fetcher: ChunkFetcher, store: BufferStore):
 
     class PostFwdPreBwd(torch.autograd.Function):
 
         @staticmethod
         def forward(ctx, params, *args):
-            ctx.params = params
-            fetcher.trans_to_hold(params, phase='f')
+            with torch._C.DisableTorchFunction():
+                ctx.params = params
+
+                fetcher.trans_to_hold(ctx.params, phase='f')
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        store.erase(p)
+
             return args
 
         @staticmethod
         def backward(ctx, *grads):
-            chunks = fetcher.trans_to_compute(ctx.params)
-            fetcher.fetch_chunks(chunks)
+            with torch._C.DisableTorchFunction():
+                chunks = fetcher.trans_to_compute(ctx.params)
+                fetcher.fetch_chunks(chunks)
+
+                offset = 0
+                for p in ctx.params:
+                    if not fetcher.is_in_fused(p):
+                        # we should add parameters to buffer
+                        # because their blocks may be changed
+                        offset = store.insert(p, offset)
+
             return (None, *grads)
 
-    return PostFwdPreBwd
+    return PostFwdPreBwd.apply

elixir/wrapper/module.py
@@ -10,8 +10,8 @@ from torch.utils._pytree import tree_map
 
 from elixir.chunk import Chunk, ChunkFetcher, ChunkGroup, MemoryPool, TensorState
 from elixir.chunk.scheduler import FIFOScheduler, PrefetchScheduler
-from elixir.hook import HookParam
-from elixir.parameter import FakeTensor, OutplaceTensor
+from elixir.hook import BufferStore, HookParam
+from elixir.parameter import OutplaceTensor
 from elixir.search import SearchResult
 from elixir.utils import gpu_device
 
@@ -55,6 +55,7 @@ class ElixirModule(nn.Module):
         self.grad_state_dict = dict()
         self.__init_chunk_group(search_result)
         self.__init_chunk_fetcher(search_result, prefetch)
+        self.__init_buffer_storage()
 
         for name, param in module.named_parameters():
             if not param.requires_grad:
@@ -132,6 +133,19 @@ class ElixirModule(nn.Module):
 
         self.fetcher = ChunkFetcher(scheduler, self.param_chunk_group, overlap=prefetch)
 
+    def __init_buffer_storage(self):
+        buffer_size = 0
+        for submodule in self.modules():
+            sum_param_size = 0
+            for param in submodule.parameters(recurse=False):
+                if not param.requires_grad or self.fetcher.is_in_fused(param):
+                    continue
+                assert param.dtype == self.dtype
+                sum_param_size += param.numel()
+            buffer_size = max(buffer_size, sum_param_size)
+        self.buffer = BufferStore(buffer_size, self.dtype)
+        print('module buffer', self.buffer)
+
     def _gradient_handler(self, grad: torch.Tensor, param: nn.Parameter):
         # create an empty tensor
         empty_grad = torch.empty_like(grad)
@@ -139,6 +153,7 @@ class ElixirModule(nn.Module):
 
         with torch._C.DisableTorchFunction():
             chunk = self.fetcher.get_one_chunk(param)
+            assert self.fetcher.group.is_accessed(chunk)
             if chunk.tensors_info[param].state != TensorState.HOLD_AFTER_BWD:
                 raise RuntimeError()
             self.fetcher.group.tensor_trans_state(param, TensorState.READY_FOR_REDUCE)
@@ -159,7 +174,7 @@ class ElixirModule(nn.Module):
 
     def forward(self, *args, **kwargs):
         self.fetcher.reset()
-        HookParam.attach_fetcher(self.fetcher)
+        HookParam.attach_fetcher(self.fetcher, self.buffer)
 
         def to_outplace_tensor(t):
             if torch.is_tensor(t):

test/wrapper/test_module.py
@@ -6,12 +6,35 @@ from test.utils import TEST_MODELS, assert_dict_values
 import pytest
 import torch
 import torch.distributed as dist
+import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.testing import assert_close
 
 from elixir.search import simple_search
 from elixir.utils import init_distributed, seed_all
 from elixir.wrapper import ElixirModule
+
+
+def check_gradient(ddp_model: nn.Module, test_model: ElixirModule):
+    name_to_grad = {name: param.grad for name, param in ddp_model.named_parameters()}
+    param_to_name = {test_model.grad_state_dict[name]: name for name in name_to_grad}
+
+    def check_chunks(cg, chunks):
+        for c in chunks:
+            cg.access_chunk(c)
+            for t in c.get_tensors():
+                name = param_to_name[t]
+                grad = name_to_grad[name]
+
+                torch.cuda.synchronize()
+                print(f'checking the gradient of parameter `{name}`')
+                assert_close(t.data, grad.data)
+
+            if not c.rcache_fused:
+                cg.release_chunk(c)
+
+    check_chunks(test_model.param_chunk_group, test_model.param_chunk_group.fused_chunks)
+    check_chunks(test_model.param_chunk_group, test_model.param_chunk_group.float_chunks)
 
 
 def exam_module_init(nproc, group, grad_flag):
@@ -21,12 +44,40 @@ def exam_module_init(nproc, group, grad_flag):
         param.requires_grad = grad_flag
     test_model = copy.deepcopy(torch_model)
 
-    sr = simple_search(test_model, nproc, 10)
+    sr = simple_search(test_model, nproc)
     model = ElixirModule(test_model, sr, group)
     # check function: ElixirModule.state_dict after ElixirModule.__init__
     torch_st = torch_model.state_dict()
     test_st = model.state_dict()
     assert_dict_values(torch_st, test_st, fn=torch.equal)
+
+
+def exam_one_module_fwd_bwd(builder, train_iter, nproc, group, exam_seed=2261):
+    ddp_model = builder().cuda()
+    test_model = copy.deepcopy(ddp_model)
+    sr = simple_search(test_model, nproc, allocate_factor=0.6)
+    test_model = ElixirModule(test_model, sr, group)
+
+    # get different data
+    seed_all(exam_seed + dist.get_rank(group))
+    data, label = next(train_iter)
+    data = data.cuda()
+
+    seed_all(exam_seed, cuda_deterministic=True)
+    ddp_model = DDP(ddp_model)
+    ddp_loss = ddp_model(data).sum()
+    ddp_loss.backward()
+
+    test_loss = test_model(data).sum()
+    test_model.backward(test_loss)
+
+    assert_close(ddp_loss, test_loss)
+    check_gradient(ddp_model.module, test_model)
+
+
+def exam_modules_fwd_bwd(nproc, group):
+    builder, train_iter, test_iter, criterion = TEST_MODELS.get_func('resnet')()
+    exam_one_module_fwd_bwd(builder, train_iter, nproc, group)
 
 
 def run_dist(rank, world_size):
@@ -38,6 +89,7 @@ def run_dist(rank, world_size):
     init_distributed()
     exam_module_init(nproc=world_size, group=dist.GroupMember.WORLD, grad_flag=False)
     exam_module_init(nproc=world_size, group=dist.GroupMember.WORLD, grad_flag=True)
+    exam_modules_fwd_bwd(nproc=world_size, group=dist.GroupMember.WORLD)
 
 
 @pytest.mark.dist
@@ -48,4 +100,4 @@ def test_elixir_module(world_size):
 
 
 if __name__ == '__main__':
-    test_elixir_module(world_size=2)
+    test_elixir_module(world_size=1)