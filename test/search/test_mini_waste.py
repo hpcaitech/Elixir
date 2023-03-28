4e35029478b225889392689954b5840d6fb33c65
HELSON <c2h214748@gmail.com>
2023-03-28 15:25:22 +0800

    [search] add unit test for optimal search

--- test/search/test_mini_waste.py
+++ test/search/test_mini_waste.py
@@ -12,7 +12,7 @@ def step_fn(model, inp):
 
 
 def test_mini_waste_search():
-    model_fn, data_fn = TEST_MODELS.get('gpt2_small')
+    model_fn, data_fn = TEST_MODELS.get('gpt2_base')
     model = model_fn()
     data = data_fn()
 
