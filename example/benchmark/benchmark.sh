d767223a51a2306a638a4a28c80001aaef3ce4d2
HELSON <c2h214748@gmail.com>
2023-04-17 18:05:02 +0800

    [benchmark] update benchmark

--- example/benchmark/benchmark.sh
+++ example/benchmark/benchmark.sh
@@ -1,6 +1,9 @@
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
 for name_model in "opt-350m" "opt-1b" "opt-3b" "opt-7b" "opt-13b"; do
     for num_gpu in 1 2 4 8; do
-        for batch_size in 8 16 24 32 40 48; do
+        for batch_size in 8 16 24 32; do
             echo "****************** Begin ***************************"
             T_MODEL=${name_model} N_GPU=${num_gpu} N_BS=${batch_size} bash ./deepspeed.sh
             T_MODEL=${name_model} N_GPU=${num_gpu} N_BS=${batch_size} bash ./elixir.sh
---
+++ example/benchmark/rm_lock.sh
@@ -0,0 +1,2 @@
+rm -rf /data2/users/lchhc/.cache/torch_extensions/py39_cu116/cpu_adam/lock
+rm -rf /data2/users/lchhc/.cache/torch_extensions/py39_cu116/fused_adam/lock
